b958fce7cc2918aee520d988d98d041f12a255b9
Kadeem Robinson <kadeem1robinson@gmail.com>
2024-07-24 23:25:30 -0400

    S7 Challenge Complete

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import * as yup from 'yup';
 
@@ -41,54 +42,64 @@ const [formErrors, setFormErrors] = useState({});
 const [formValid, setFormValid] = useState(false);
 const [formSuccess, setFormSuccess] = useState('');
 const [formFailure, setFormFailure] = useState('');
+const [message, setMessage] = useState()
 
 useEffect(() => {
-formSchema
-.validate(formValues, { abortEarly: false })
-.then(() => {
-  setFormValid(true);
-  setFormErrors({});
-})
-.catch(err => {
-  setFormValid(false);
-  const newErrors = {};
-  err.inner.forEach(error => {
-    newErrors[error.path] = error.message;
+  formSchema.isValid(formValues).then((isValid) => {
+    setFormValid(isValid);
   });
-  setFormErrors(newErrors);
-});
 }, [formValues]);
-
+console.log(formValues)
 const handleSubmit = (evt) => {
 evt.preventDefault();
-formSchema
-.validate(formValues)
-.then(() => {
-setFormSuccess(true);
-setFormFailure(false);
-setFormValues({
-fullName: '',
-size: '',
-toppings: Object.fromEntries(toppings.map(topping => [topping.topping_id, false])),
-})
-})
-.catch(() => {
-setFormFailure(true);
-setFormSuccess(false);
-});
+const newFormValues = {
+  ...formValues, 
+  toppings: Object.keys(formValues.toppings).filter(toppingId => formValues.toppings[toppingId])
+}
+axios
+  .post('http://localhost:9009/api/order', newFormValues)
+  .then((res) => {
+    console.log(res.data)
+    setMessage(res.data.message)
+    setFormSuccess(true)
+    setFormValues({fullName: '',
+    size: '',
+    toppings: Object.fromEntries(toppings.map(topping => [topping.topping_id, false]))})
+  })
+  .catch((err) => err.message)
+    setFormFailure()  
 };
 
-const handleChange = (toppingId) => {
-setFormValues({
-...formValues,
-toppings: { ...formValues.toppings, [toppingId]: !formValues.toppings[toppingId] },
-});
+const handleChange = (evt, toppingId) => {
+  let { type, name, value } = evt.target;
+    if (type === "checkbox") {  
+      setFormValues({
+      ...formValues,
+      toppings: { ...formValues.toppings, [toppingId]: !formValues.toppings[toppingId] },
+      });
+    }
+    else {
+    setFormValues({ ...formValues, [name]: value });
+    // The ".reach()/.validate()" combination allows you to check a single value
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then(() => {
+        // If value is valid, the corresponding error message will be deleted
+        setFormErrors({ ...formErrors, [name]: "" });
+      })
+      .catch((err) => {
+        // If invalid, we update the error message with the text returned by Yup
+        // This error message was hard-coded in the schema
+        setFormErrors({ ...formErrors, [name]: err.errors[0] });
+      });
+    }
 };
 
 return (
 <form onSubmit={handleSubmit}>
 <h2>Order Your Pizza</h2>
-{formSuccess && <div className='success'>Thank you for your order!</div>}
+{formSuccess && <div className='success'>{message}</div>}
 {formFailure && <div className='failure'>Something went wrong</div>}
 
 <div className="input-group">
@@ -97,9 +108,10 @@ return (
 <input
 placeholder="Type full name"
 id="fullName"
+name='fullName'
 type="text"
 value={formValues.fullName}
-onChange={(evt) => setFormValues({ ...formValues, fullName: evt.target.value })}
+onChange={(evt) => handleChange(evt)}
 />
 </div>
 {formErrors.fullName && <div className='error'>{formErrors.fullName}</div>}
@@ -110,8 +122,9 @@ onChange={(evt) => setFormValues({ ...formValues, fullName: evt.target.value })}
 <label htmlFor="size">Size</label><br />
 <select
 id="size"
+name='size'
 value={formValues.size}
-onChange={(evt) => setFormValues({ ...formValues, size: evt.target.value })}
+onChange={(evt) => handleChange(evt)}
 >
 {/* Fill out the missing options */}
 <option value="">----Choose Size----</option>
@@ -131,7 +144,7 @@ onChange={(evt) => setFormValues({ ...formValues, size: evt.target.value })}
 name={topping.topping_id}
 type="checkbox"
 checked={formValues.toppings[topping.topping_id] || false}
-onChange={() => handleChange(topping.topping_id)}
+onChange={(evt) => handleChange(evt, topping.topping_id)}
 />
 {topping.text}<br />
 </label>
